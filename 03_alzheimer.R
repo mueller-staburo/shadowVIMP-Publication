d392bd22575aaeef525bd6262304fc066645273c
Tim Müller <mueller@staburo.de>
2025-06-20 10:51:22 +0200

    updates to 03_alzheimer.R (documentation)

--- 03_alzheimer.R
+++ 03_alzheimer.R
@@ -1,19 +1,22 @@
+#Loading all functions
 lapply(list.files(path = "./functions", full.names= T), source)
 
+#Loading all libraries and the Alzheimer disease data set and setting a seed
 library(AppliedPredictiveModeling)
 data(AlzheimerDisease)
 library(ggplot2)
 library(dplyr)
 library(tidyverse)
-
-
 set.seed(1807)
 
+#Preprocessing
 data_alz <- cbind(diagnosis, predictors) %>% rename(y = diagnosis)
 data_alz$y <- as.factor(data_alz$y)
 data_alz$male <- as.factor(data_alz$male)
 
-
+#Applyying shadowvimp with preselection; note that the num.threads parameter (the number of cores the ranger packag
+#can use for multithreading may need adjusting on your system. respect.unordered.factors is passed to the ranger function
+#so categegorical variables are properly used in building the trees)
 alz_three_steps <- vim_perm_sim_wrapper(nsims= c(30, 120, 1000),
                                         alphas = c(0.3, 0.15, 0.05),
                                         entire_data = data_alz,
@@ -22,6 +25,7 @@ alz_three_steps <- vim_perm_sim_wrapper(nsims= c(30, 120, 1000),
                                         respect.unordered.factors = "order",
                                         y = y)
 
+#save the result
 saveRDS(alz_three_steps, "alzheimer.rds")
 
 
@@ -38,7 +42,7 @@ sum(stats::p.adjust(1-named_pvals, method = 'BH', n = 130) <= 0.05)
 sum(stats::p.adjust(1-named_pvals, method = 'holm', n = 130) <= 0.05)
 sum(1-named_pvals <= 0.05)
 
-#vims
+#Plotting importances and result of shadowVIMP
 plot_vimps <- function(wrapper_object, pooled = T, ...) {
 max_step <- wrapper_object %>% length()
 
@@ -62,35 +66,13 @@ ggplot(vimps, aes(y=reorder(varname, VIMP, FUN = median), x = VIMP, fill= decisi
 
 }
 
-plot_shadows <- function(wrapper_object, pooled = T, ...) {
-  max_step <- wrapper_object %>% length()
-  
-  decisions <- wrapper_object[[max_step]][["vimpermsim"]][["test_results"]][["with_correction"]][[ifelse(pooled, "pooled", "per_variable")]][["0_05"]] %>% select(c("varname", "Type1_confirmed","FDR_confirmed","FWER_confirmed"))  %>%
-    mutate(decision = factor(x = case_when( (FWER_confirmed == 1)&(FDR_confirmed == 1)&(Type1_confirmed == 1)~ "FWER conf.",
-                                            (FWER_confirmed == 0)&(FDR_confirmed == 1)&(Type1_confirmed == 1)~ "FDR conf.",
-                                            (FWER_confirmed == 0)&(FDR_confirmed == 0)&(Type1_confirmed == 1)~ "Unadjusted conf.",
-                                            (FWER_confirmed == 0)&(FDR_confirmed == 0)&(Type1_confirmed == 0)~ "Not significant"
-    ), levels = c("FWER conf.", "FDR conf.", "Unadjusted conf.", "Not significant"), ordered = T))%>%
-    select(-c( "Type1_confirmed","FDR_confirmed","FWER_confirmed"))
-  
-  
-  
-  vimps <- wrapper_object[[max_step]][["vimpermsim"]][["vim_simulated"]] %>% 
-    tidyr::pivot_longer(cols = everything(), names_to = "varname", values_to = "VIMP") %>%
-    filter(grepl('_permuted', varname)) %>% left_join(decisions, by="varname")
-  
-  
-  ggplot(vimps, aes(y=reorder(varname, VIMP, FUN = median), x = VIMP, fill= decision), ...) + geom_boxplot(outlier.size = 0.5)  + 
-    #scale_x_discrete(labels = function(labels) str_replace_all(labels, "_", " ")) +
-    #stat_summary(fun = function(y) quantile(y, 0.995), geom = "point", color = "red", size = 3) + 
-    theme_bw() + theme(...)
-}
 
+#Producing plots (Figure 2)
 plot_vimps(alz_three_steps, axis.title=element_blank(), legend.position = "none", text=element_text(size=20))
-plot_shadows(alz_three_steps, axis.title=element_blank(), legend.position = "none", text=element_text(size=20))
 
 
-#
+
+#Producing base RF VIMP plot as displayed in introduction (Figure 1)
 set.seed(1807)
 base_rf <- ranger::ranger(formula = y~., data=data_alz, num.trees=10000, importance="permutation", scale.permutation.importance = T)
 vimps_baserf <- data.frame(VIMP = base_rf$variable.importance, varname = base_rf$variable.importance %>% names()) %>% arrange(desc(VIMP))
